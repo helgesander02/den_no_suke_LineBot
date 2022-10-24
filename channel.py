2e2c6cf701089c3aaef0d431b0da837821057ab4
helgesander02 <98319298+helgesander02@users.noreply.github.com>
2022-10-24 13:32:33 +0800

    Update channel.py

--- channel.py
+++ channel.py
@@ -1,4 +1,4 @@
 keys = {
-  "Chennel_access_token":"",
-  "Channel_secret":""
+  "Chennel_access_token":"Lao5R8mcYoe+afUtB/o+9KiKB3NJ4XMsBA2cG4zmTRm1b+7DUwXdA0kNkYxL2DQPZ3fD+RXOgjtCk1DBJ1KNS+ZtNVZt6OtOSm2QFnWYnE5pbrYkJXMJ6lEf4KkdOIYMVPOMorxiltAKskfrcShiAQdB04t89/1O/w1cDnyilFU=",
+  "Channel_secret":"1508a7544118481b7d46b298eff6a48e"
 }
