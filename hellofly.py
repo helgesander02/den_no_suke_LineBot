72e6f101ecbe71b80f6a1f583dc2a7fe64a1e025
helgesander02 <98319298+helgesander02@users.noreply.github.com>
2022-10-24 22:32:02 +0800

    Update hellofly.py

--- hellofly.py
+++ hellofly.py
@@ -1,31 +1,46 @@
-from __future__ import unicode_literals
-import os
 from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from channel import keys
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
 
 app = Flask(__name__)
 
-# LINE 聊天機器人的基本資料
-line_bot_api = LineBotApi(keys["Chennel_access_token"])
-handler = WebhookHandler(keys["Channel_secret"])
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 
-# 接收 LINE 的資訊
+
 @app.route("/callback", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
+    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
+    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
 
     return 'OK'
 
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
 if __name__ == "__main__":
     app.run()
-
