e5b9358238bcaa228c55dd747ec09b530e415c16
helgesander02 <98319298+helgesander02@users.noreply.github.com>
2022-11-19 14:08:26 +0800

    Delete channel.py

--- channel.py
+++
@@ -1,4 +0,0 @@
-keys = {
-  "Chennel_access_token":"ip5I3GLs4SY1VoTdyba6/fSu5Ykf57CJU8OpuquGppUPx7oHC3OR6KGb/itRL3b6anTqR61HMdBmEpv99JMmrzf9BJjTGzhyhaU4Bz9GcGOv6pC/aFvfOewJF9WN6iZAuQzAUYIE4mSKpwHWvBWrLAdB04t89/1O/w1cDnyilFU=",
-  "Channel_secret":"4f7a42505f09c83b2680dcae8884fcc8"
-}
