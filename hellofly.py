077a681018ef87343ca02c47bfdcc13ffde545a4
helgesander02 <98319298+helgesander02@users.noreply.github.com>
2022-12-11 16:29:44 +0800

    Update hellofly.py

--- hellofly.py
+++ hellofly.py
@@ -41,7 +41,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = str(event.message.text)
-    if msg == "隨機一組數字":
+    if msg == "餵食電之助":
         x = random.randrange(100)
         line_bot_api.reply_message(
             event.reply_token,
@@ -108,9 +108,9 @@ def handle_message(event):
         
     else:
         
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="根據問題深度需要1至3分鐘"))
+        #line_bot_api.reply_message(
+            #event.reply_token,
+            #TextSendMessage(text="根據問題深度需要1至3分鐘才會回答"))
         
         session_token = "eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..PvSL4cZdtVx0jxcf.pP-5BydOd3QrNrGh6BScYDSLhMIcGpKXq3JhB7z7bwf7XhmeHP1FiM9uWhOz1swJ2em9YKBX29VQhljoID9eeez57yk9GJO-M3SvwNQYYVpfWzz7fIun5tS-OnOWw2zbMpJ6u_ffnAdZWEKaSJXquMKgU-qJo1H80Ohwn-qkvN_8vX-CzYoFkR0RIPqyRL0VVdCPVpwHnBgBAfo4Lp4iJFiyBR2IF9LjFWosgOy-hJFYJxRDiWvR1CjOqfuc9tb7PaYUhAmday42MZtSZWsfGw-rweLiE1zLbkRiIQNSxxdyi9cmaYtSHChvSSEK0WlKGlVVTtMZdfKwuO9BGWNfe8-udwBHHGpEYXm0ZDzshv5nTbg7pUPFCnA4ILMG1gOuK5jCZl73CtuR6D1MGoxBK6G702mmiVjMNmzGxFVNrEBU6vw590swE0YOVfQ0HMySVeQUhKxXrAZN5JR36Z5oM7YczAB7Jq6cXGKPz4u0dVZTx0sb7FZYP1BS0isAftvFaxIa2C4aFJ9gcbOmJWAYXEv4JehcHi8LgMSrag62VojVOQ2xuvywbreWUlgdukK7w5MCkFEtj6kIeUZ4M-6H6tfkDZQgroEsjuwr_-Q3_emnk00BVeZn6D81jkEE1GWj_vIAjzpJUJf3fFBsVVE8zGO_UKGoXDJn3OBWWTo9V_vGi8BPadFFNrZiyv-OriL8cjithcUn88CudME5sfGNfiMg9IqxUFtJXms9CRES9MVrqcIqMuJ6ocLHx7efUMRzcWYFArfdg5BX0kdNUgq6fy-bM2X2uas3qRNHgfm7tiVigefIy0D9P5aN_SPOfWpUGedJZpuEtypJj8i8woahwhZWDhJYnk5LjqQj2qvyEiOLXQ22ezAfOJkgRaXtkdPNGNdu52txgm72hBZS1q6K38alxhufuUJ13xz7Cs19uhnhuKRhVFfFMKrmpJrZdagCn_xX6O6T2oR7MggX3Rb-0RdlOUt2ba9IcAdVXooz6F6Q1JxHQnG0y9jVdyHGJ8Uo5fNrARbAoJJta_lb3Lljwz_FSpVkwebffV3W3s5O80a8wVlHmZ1AkwtKK0-shdgRhelL1kg3bEXdIM_IekELfZtcAi-rQFg_gmdYDs9c1Z1g3cU7BqjVjtq6BCtpU3anuN2A7Uu-0L94ZzWtaQSfrby7ALLGeX_to5ENPGA-3XPVN8_r-EGfUI5vUs9oPu9XBUl_Pr1sjRUmmrXp7jZzg9-l7OUJB8ZQR6y9vH1o_4k1WdFtBsNF7Kn96y8yQeM9gfhw0axrmLEv1npxdFsI56uJGTiSzBVtDygsBPHBBNP3GcvaF0C5x2tO9YTxNJOC41zuNlhOASpq8zVNEt8KHQiwCHMNLQVYGw_J-WcWRZCSUnrlXVi_TuOnUJuY5EBGY1S86AC3lqdy3-mE8iAPefBzq67KmrdjXHts8L-1OheZBZPp6Mr3GazHqGaYKCuN2WYo7dkPjUrwGVSjXmV_f8zgvwprB59_d4VXeHT3Z4OnF3ADsXh4XgU2H4cG76c9LIWUo5tZEnKdMr4vnnnJ6OXtz6Azvxde4yNLjUzaS9zW3L_CrRI7GCtG_gUg3J0l7-o67sMTzWRZUeUX6Ssgge9mb96-Hd_vPlQyCC1cBovr9Lbdre85VIVyNQqhgRWxeIh3fc3GGOinP-cMgch6IOB2WQzjOuioVcW6orRavDSci0nAhE9EQ-tPjnnQ_5rKiDfCAfBZg604VWjNWiaSZnwR0AqGo9QmHu1vqgCYj0ERG2k3WMltSLje_PjZnK8SIZwlvqBiil6bBr85_wBhL5AtnNXOIgjc55FSWRSBVPi8mGA4yiA737yiPb6QutfqI3CjqUsF1lA4U82SnGXaS_cC21Urk3ww4pSuCCqEiO8L8UkQXyjIAga-1d2QuNkKFULAPFIjgQcDXN7jVGvFk3f-IlRvjp7vTYJ7lQqXVBTEXw9mg8IkpUCpGfOV4ACejwOF6Lxj08CnPz_6h_9Qc14cX2I-62AGaB0lYrRv6sOvD3QxuvpQVNy1DHcF8wMiEcr_dp0nV10HyqXmnyu39up2OSIvNQt-V50UWUcVWIquw35WoSEYsFFqYj7LNACH1oQIQFsS2wuquEP57TRk5J-lmuiuQhenjc5lR0HhZscTioXhIx5HvRPwVhjbT2dbGHpIJUQj9iwAzyDYfNwkVhftQ9EVQndtZhgiQjmdhq3ZtvKxt0SHBLnfgcEDig6s9SAe_q6DuYSi.C2d9LRY76viR41MKo4YQOA"
         api = ChatGPT(session_token)
