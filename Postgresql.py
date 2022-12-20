edd7b02aab090e84b9b013eb142f796828a54112
helgesander02 <98319298+helgesander02@users.noreply.github.com>
2022-12-20 23:12:34 +0800

    Create Postgresql.py

---
+++ Postgresql.py
@@ -0,0 +1,22 @@
+import psycopg2
+
+
+class database:
+    def __init__(self, user_name, uid):
+        self.user_name = user_name
+        self.uid = uid
+        self.Internal_Database_URL = "postgres://su:IqObEwxiLHJbo7aRzb8lyyOKiWBcVzfU@dpg-cegsd914rebaribgjg30-a/dennosuke_vsns"
+        
+    def add_food(self):
+        conn = psycopg2.connect(self.Internal_Database_URL)
+        cur = conn.cursor()
+        cur.execute(f"SELECT ID, NAME, COUNT FROM DENNOSUKE WHERE ID='{self.uid}'")
+        rows = cur.fetchall()
+        if rows == []:
+            cur.execute(f"INSERT INTO DENNOSUKE (ID, NAME, COUNT) \
+                        VALUES ('{self.uid}','{self.user_name}', 1)");
+        else:
+            cur.execute(f"SELECT COUNT FROM DENNOSUKE WHERE ID='{self.uid}'")
+            c = cur.fetchall()[0][0]
+            cur.execute(f"UPDATE DENNOSUKE set COUNT = {c+1} where ID='{self.uid}'")
+
